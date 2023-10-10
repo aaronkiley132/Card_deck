d402a6e75cf8e6c80c95437ae4bcc6065a2234d6
18kiley_a <18kiley_a@bexleygs.co.uk>
2023-10-10 14:12:13 +0100

    V1

--- src/Deck.java
+++ src/Deck.java
@@ -16,6 +16,20 @@ public class Deck {
         }
     }
 
-    public void sortAlphabetically() {
+    public void sortAlphabetically() { //bubble sort to sort the cards alphabetically
+        String cardOne;
+        String cardTwo;
+        Card x = new Card(" ", 0);
+        boolean swaps = true;
+        int counter = 0;
+        while ((counter < size) && (swaps = true)) {
+            for (int i = 0; i < (size - 1 - counter); i++) {
+                cardOne = deck[i].toString();
+                cardTwo = deck[i + 1].toString();
+                if (String.compareTo(cardOne, cardTwo)){
+
+                }
+            }
+        }
     }
 }
