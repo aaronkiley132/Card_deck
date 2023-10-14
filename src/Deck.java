264675029eb8b4052d018853ea790530f5b146b2
aaronkiley132 <139629281+aaronkiley132@users.noreply.github.com>
2023-10-14 17:15:11 +0000

    should be finished though there may be a slight logic error in the pontoon implementation

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Deck {
 
     private Card[] deck;
@@ -26,13 +28,11 @@ public class Deck {
         String cardTwo;
         boolean swaps = true;
         int counter = 0;
-        int comparison;
         while ((counter < size) && (swaps = true)) {
             for (int i = 0; i < ((size - 1) - counter); i++) {
               swaps = false;
               Card x = new Card(" ", 0);
-              comparison = deck[i].getValue() - deck[i+1].getValue();
-              if (comparison > 0){
+              if (deck[i].compareTo(deck[i+1]) > 0){
                 x = deck[i];
                 deck[i] = deck[i+1];
                 deck[i+1] = x;
@@ -67,6 +67,42 @@ public class Deck {
       counter++;
     }
   }
+
+  public void shuffle(){
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++) {
+      Card x = new Card("", 0);
+      int randomIndexToSwap = rand.nextInt(size);
+      x = deck[randomIndexToSwap];
+      deck[randomIndexToSwap] = deck[i];
+      deck[i] = x;
+    }
+  }
+
+  //deals a hand of x to y players
+  public Card[][] deal(int hand, int players){
+    Card[][] output = new Card[players][hand];
+    this.shuffle();
+    int counter = 0;
+    for (int i = 0; i < hand; i++){
+      for (int j = 0; j < players; j++){
+        output[j][i] = deck[counter];
+        counter++;
+      }
+    }
+
+    // display hands
+    for (int i = 0; i < players; i++){
+      //System.out.println("Player " + (i + 1) + ":");
+      for (int j = 0; j < hand; j++){
+        System.out.print(output[i][j].toString() + ", ");
+      }
+      System.out.println("\n");
+    }
+    return output; 
+  }
+
 
     //returns a string of the deck
     public void displayDeck(){
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,54 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Card cardOne = new Card("Hearts", 7);
-        Card cardTwo = new Card("Hearts", 7);
-        System.out.println(cardOne.compareTo(cardTwo));
+        Deck myDeck = new Deck();
+        // pontoon
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("How many players are there?");
+        int playerCount = Integer.parseInt(myScanner.nextLine());
+        int cardNumber;
+        boolean aces;
+        // players
+        // players
+        int[] sums = new int[playerCount];
+        Card[][] cards = new Card[1][53];
+        for (int i = 0; i < playerCount; i++){
+          // deal however many cards each player wants
+          System.out.println("Player" + (i+1));
+          System.out.println("How many cards do you want?");
+          cardNumber = Integer.parseInt(myScanner.nextLine());
+          cards = myDeck.deal(cardNumber, 1);
+          // aces high or low
+          System.out.println("Do you want aces to be high?");
+          aces = Boolean.parseBoolean(myScanner.nextLine());
+        // sum the value of these
+          for (int j = 0; j < cards[0].length; j++){
+            if (cards[0][j].getValue() == 1){
+              if (aces == true){
+                sums[i] += 11;
+              }
+              else{
+                sums[i] += 1;
+              }
+            }else if(cards[0][j].getValue() >= 11){
+              sums[i] += 10;
+          }else{
+              sums[i] += cards[0][1].getValue();
+            }
+        // repeat for each player
+        }
+        }
+      // find the highest card in the array (that is under 21)
+      int index = -1;
+      int highest = -1;
+      for (int i = 0; i < sums.length; i++){
+        if (sums[i] > highest && sums[i] <= 21){
+          index = i;
+        }
+      }
+      // output the player that has won (i+1)
+        System.out.println("Player " + (index+1) + " wins!");
         }
     }
